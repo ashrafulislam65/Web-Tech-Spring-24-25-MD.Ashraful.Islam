a6fc2b4217c2682dddc04c3454a4a1c2698dccfb
ashrafulislam65 <md.ashraful.islam7758a@gmail.com>
2025-05-05 17:02:06 +0600

    employee list table added partialy and delete functionality added

---
+++ Php-Tutorial/first.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello</h1>
+    <?php
+     echo("Hello")
+    ?>
+    <?php
+       $first_Name = "Mr.x";
+       $second_Name="Il";
+       echo "Full Name:".$First_Name.$second_Name;
+    ?>
+</body>
+</html>
